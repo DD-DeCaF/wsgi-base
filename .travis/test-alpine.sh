825c8015388fa88b293e030a1832ae3b9c93175f
Ali Kaafarani <ali@kvikshaug.no>
2019-11-08 13:08:15 +0100

    chore: run test suite based on the built image base

---
+++ .travis/test-alpine.sh
@@ -0,0 +1,8 @@
+set -eux
+cookiecutter --no-input https://github.com/DD-DeCaF/cookiecutter-flask-microservice
+pushd name-of-the-project
+make pip-compile
+# The generated requirements file is initially owned by root, so update ownership.
+sudo chown -R travis .
+make network build qa
+popd
