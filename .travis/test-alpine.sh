4da44ec55575346ed1991c701c06a29a3e9481d2
Ali Kaafarani <ali@kvikshaug.no>
2019-11-12 12:21:52 +0100

    docs: describe wsgi-base vs cookiecutter test intentions

--- .travis/test-alpine.sh
+++ .travis/test-alpine.sh
@@ -1,4 +1,12 @@
 set -eux
+
+# Test the image (alpine packages + python dependencies) by creating a service
+# on top of the base image that was just built, and run the QA suite.
+# Note that we're using the master branch of the cookiecutter repo, which has
+# already passed QA. Hence, we're not testing the state of the cookiecutter
+# here - just that it works with the new dependencies installed in the base
+# image.
+
 cookiecutter --no-input https://github.com/DD-DeCaF/cookiecutter-flask-microservice
 pushd name-of-the-project
 make pip-compile
